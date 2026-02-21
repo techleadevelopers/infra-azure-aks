f5f299ddd8adcb7f7cc6b92291ba801cde5ce734
devopscomputer <pauloti.engenharia@gmail.com>
2026-02-21 14:23:00 -0300

    update: update main.tf

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ provider "azurerm" {
   features {}
 }
 
-# 2. Grupo de Recursos (Container lógico)
+# 2. Grupo de Recursos
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
 }
 
-# 3. Rede Virtual (VNet) - Onde a mágica acontece
+# 3. Rede Virtual (VNet) e Subnet
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.project_name}"
   address_space       = ["10.0.0.0/16"]
@@ -27,7 +27,6 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
-# Subnet específica para o AKS
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "snet-aks"
   resource_group_name  = azurerm_resource_group.main.name
@@ -43,23 +42,27 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.project_name
 
   default_node_pool {
-    name           = "default"
-    node_count     = var.node_count
-    vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.aks_subnet.id
-    
-    # Habilita o auto-scaling dos nós (Papo de Sênior)
+    name                = "default"
+    node_count          = 2
+    vm_size             = "Standard_DC2s_v3" # VM disponível na sua cota
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
     enable_auto_scaling = true
     max_count           = 5
     min_count           = 1
   }
 
-  # Identidade Gerenciada (Segurança: sem senhas hardcoded)
   identity {
     type = "SystemAssigned"
   }
 
-  # Integração com Azure Monitor / Log Analytics (Observabilidade)
+  # RESOLVE O ERRO DE OVERLAP (Conflito de IP)
+  network_profile {
+    network_plugin     = "azure"
+    service_cidr       = "172.16.0.0/16" # IPs dos serviços (fora da VNet 10.0)
+    dns_service_ip     = "172.16.0.10"   # IP do DNS interno
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+
   oms_agent {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
   }
@@ -70,7 +73,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# 5. Log Analytics (Para o Grafana/Prometheus da vaga lerem depois)
+# 5. Log Analytics (Observabilidade)
 resource "azurerm_log_analytics_workspace" "insights" {
   name                = "logs-${var.project_name}"
   location            = azurerm_resource_group.main.location
